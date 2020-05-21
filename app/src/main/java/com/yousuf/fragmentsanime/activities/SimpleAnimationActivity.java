8e4d4d02b6382db87d5d573e3e5d56b8bf1b0134
Yousuf <josephali1993@gmail.com>
2020-05-21 11:58:29 +0400

    #SimpleAnimationActivity :  +implements MediatorInterface
    +FirstFragment
    +SecondFragment
    +ThirdFragment

--- app/src/main/java/com/yousuf/fragmentsanime/activities/SimpleAnimationActivity.java
+++ app/src/main/java/com/yousuf/fragmentsanime/activities/SimpleAnimationActivity.java
@@ -3,14 +3,33 @@ package com.yousuf.fragmentsanime.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.yousuf.fragmentsanime.R;
+import com.yousuf.fragmentsanime.fragments.FirstFragment;
+import com.yousuf.fragmentsanime.interfaces.MediatorInterface;
 
-public class SimpleAnimationActivity extends AppCompatActivity {
+public class SimpleAnimationActivity extends AppCompatActivity implements MediatorInterface {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changeFragmentTo(new FirstFragment(), FirstFragment.class.getSimpleName());
+    }
+
+    @Override
+    public void changeFragmentTo(Fragment fragmentToDisplay, String tag) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.replace(R.id.fl_host, fragmentToDisplay, tag);
+        if (fm.findFragmentByTag(tag) == null) {
+            ft.addToBackStack(tag);
+        }
+        ft.commit();
     }
 }
---
+++ app/src/main/java/com/yousuf/fragmentsanime/fragments/ThirdFragment.java
@@ -0,0 +1,34 @@
+package com.yousuf.fragmentsanime.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.yousuf.fragmentsanime.R;
+
+public class ThirdFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+
+
+/*    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }*/
+
+}
